6ae465866cdb7fecd930000bc5a9af7f838bb8e0
sworduck <iwanovmicha6@gmail.com>
2022-06-06 17:23:16 +0400

    commit

--- app/src/main/java/com/example/swapi/CharacterDb.kt
+++ app/src/main/java/com/example/swapi/CharacterDb.kt
@@ -1,4 +1,22 @@
 package com.example.swapi
 
-class CharacterDb {
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+
+open class CharacterDb(): RealmObject(){
+    constructor(_id:Int,_name:String,_height:String,_mass:String,_homeworld:String):this(){
+        id = _id
+        name = _name
+        height = _height
+        mass = _mass
+        homeworld = _homeworld
+    }
+    @PrimaryKey
+    var id = 0
+
+    var name = "0"
+    var height = "0"
+    var mass = "0"
+    var homeworld = "0"
+    //fun map(mapper: CharacterCacheMapper) = CharacterData(id,name,height,mass,homeworld)
 }
